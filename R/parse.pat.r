c9e2f411592b94e13759fdef92de6c8f6aed949a
Wesley Brooks <brooks.wesley@gmail.com>
2016-10-12 21:56:19 +1100

    Reflecting the styling for PATs in new play-by-play code

--- R/parse.pat.r
+++ R/parse.pat.r
@@ -1,30 +1,22 @@
 parse.pat = function(pbp, play) {
     play$PAT = FALSE
 
-    pat_regex1 = paste("(?<kicker>[-a-zA-Z\\. ']+) (extra point|KICK)",
-        " (?<made>GOOD|MADE)|(?<missed>MISSED|NO GOOD)", sep='')
-    pat_regex2 = paste("\\((?<kicker>[-a-zA-Z\\. ']+) (extra point|KICK)",
-        "( (?<made>GOOD|MADE)|(?<missed>MISSED|NO GOOD))?\\)", sep='')
+    # pat_regex1 = paste0("(?<kicker>[-a-zA-Z,\\. ']+)",
+    #     " (extra point|KICK|PAT)",
+    #     "( (?<made>GOOD|MADE)| (?<missed>MISSED|NO GOOD|BLOCKED))")
+    pat_regex = paste0("(TD|touchdown)[^[:alnum:]]* \\(?(?<kicker>[-a-zA-Z,\\. ']+)",
+        " (extra point|KICK|PAT)",
+        "( (?<made>GOOD|MADE)| (?<missed>MISSED|NO GOOD|BLOCKED))?\\)?")
 
-    if (grepl(pat_regex1, pbp, perl=TRUE, fixed=FALSE, ignore.case=TRUE)) {
-        play$PAT = TRUE
-        play$made = TRUE
+    if (grepl(pat_regex, pbp, perl=TRUE, fixed=FALSE, ignore.case=TRUE)) {
+        play$PAT <- TRUE
+        play$made <- TRUE
         
-        match = regex(pat_regex1, pbp, perl=TRUE, fixed=FALSE, ignore.case=TRUE)
-        play$kicker = match[1,'kicker']     
+        match <- regex(pat_regex1, pbp, perl=TRUE, fixed=FALSE, ignore.case=TRUE)
+        play$kicker <- format.name(match[1,'kicker'])     
            
         if (!is.na(match[1,'missed'])) {
-            play$made = FALSE
-        }
-    } else if (grepl(pat_regex2, pbp, perl=TRUE, fixed=FALSE, ignore.case=TRUE)) {
-        play$PAT = TRUE
-        play$made = TRUE
-        
-        match = regex(pat_regex2, pbp, perl=TRUE, fixed=FALSE, ignore.case=TRUE)
-        play$kicker = match[1,'kicker']     
-           
-        if (!is.na(match[1,'missed'])) {
-            play$made = FALSE
+            play$made <- FALSE
         }
     }
     
