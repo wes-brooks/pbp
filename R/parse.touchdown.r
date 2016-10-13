1250178989752c9255fb98c28fc17ce934dce150
Wesley Brooks <brooks.wesley@gmail.com>
2016-10-14 08:36:51 +1100

    Altered the matching pattern for play-by-play that doesn't explicitly identify touchdown plays.

--- R/parse.touchdown.r
+++ R/parse.touchdown.r
@@ -1,10 +1,10 @@
-parse.touchdown = function(pbp, play) {
-    play$TD = FALSE
+parse.touchdown <- function(play) {
+    play$TD <- FALSE
     
-    td_regex = "(?<touchdown>touchdown|TD)"
+    td_regex <- paste0("(?<touchdown>touchdown|TD)|\\(", name.pattern, "(kick|PAT)( (good|missed|blocked|no good))?\\)")
     
-    if (grepl(td_regex, pbp, perl=TRUE, fixed=FALSE, ignore.case=TRUE)) {
-        play$TD = TRUE
+    if (grepl(td_regex, play$pbp, perl=TRUE, fixed=FALSE, ignore.case=TRUE)) {
+        play$TD <- TRUE
     }
     
     return(play)
